4c93f17d31d0c752d684e34d3a7eb1b0689022a3
etamar <etamar.levy@gmail.com>
2021-10-12 22:16:38 -0400

    commit tree 5a684784f51ee760625c9ce893515e133825e688

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity_error
+  before_action :authorize
 
+
+  def authorize
+    @user = User.find_by(id: session[:user_id])
+    render json: { errors: ["Not authorized"] }, status: :unauthorized unless @user
+  end
+
+
+  def unprocessable_entity_error(problem)
+    render json: { errors: problem.record.errors.full_messages }, status: :unprocessable_entity
+  end
 end
